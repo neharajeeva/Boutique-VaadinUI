22717419b98fad597a1963bb255e32435643c1ce
neharajeeva <156348434+neharajeeva@users.noreply.github.com>
2024-11-20 13:55:16 -0500

    Delete analysis directory

--- analysis/src/main/java/com/boutique/analysis/repositories/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boutique.analysis.repositories;
-
-import com.boutique.analysis.entities.ClientEntity;
-import org.springframework.data.repository.CrudRepository;
-import java.util.Optional;
-
-public interface ClientRepository extends CrudRepository<ClientEntity, Integer> {
-    Optional<ClientEntity> findByClientNameIgnoreCase(String clientName);
-}
